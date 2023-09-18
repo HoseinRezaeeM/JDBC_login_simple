7d8f2ecfd79002b006d4ef8d5ca482d0d961ad3b
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-18 17:40:08 +0330

    Create Category class and write properties getter setter and constructor

---
+++ src/main/java/models/Category.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Category {
+    private Integer idCategory;
+    private String name;
+    private String description;
+
+    public Category(Integer idCategory, String name, String description) {
+        this.idCategory = idCategory;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(Integer idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
