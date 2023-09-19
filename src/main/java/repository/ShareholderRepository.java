1f15fb43ca7c2994e1f739aa788b0e7511e4c5e2
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-19 15:13:31 +0330

    write update and delete method in ShareholderRepository

--- src/main/java/repository/ShareholderRepository.java
+++ src/main/java/repository/ShareholderRepository.java
@@ -14,14 +14,36 @@ public class ShareholderRepository {
     public ShareholderRepository(Connection connection) {
         this.connection = connection;
     }
-    public int save(Shareholder shareholder)throws SQLException{
-        String sql="INSERT INTO shareholder(name, phonenumber, nationacode) VALUES (?,?,?)";
+
+    public int save(Shareholder shareholder) throws SQLException {
+        String sql = "INSERT INTO shareholder(name, phonenumber, nationacode) VALUES (?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,"name");
-        preparedStatement.setString(2,"phoneNumber");
-        preparedStatement.setString(3,"nationalCode");
+        preparedStatement.setString(1, "name");
+        preparedStatement.setString(2, "phoneNumber");
+        preparedStatement.setString(3, "nationalCode");
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
+
+    public int updateInfoShareholder(String name, String phoneNumber, String nationalCode, int idShareholer) throws SQLException {
+        String sql ="UPDATE shareholder SET name=?, phonenumber=?, nationacode=? WHERE idshareholder=?";
+        PreparedStatement preparedStatement =connection.prepareStatement(sql);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,phoneNumber);
+        preparedStatement.setString(3,nationalCode);
+        preparedStatement.setInt(4,idShareholer);
         int result =preparedStatement.executeUpdate();
         return result;
+
+
+    }
+    public int delete(int idShareholder)throws SQLException{
+        String sql="DELETE FROM shareholder WHERE idshareholder=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setInt(1,idShareholder);
+        int result=preparedStatement.executeUpdate();
+        return result;
+
     }
 
 }
