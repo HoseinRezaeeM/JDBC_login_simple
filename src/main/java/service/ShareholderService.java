20469ac40c0f8684efc6c30ba6e345844dfd60d9
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-19 15:53:14 +0330

    write delete method in ShareholderService class

--- src/main/java/service/ShareholderService.java
+++ src/main/java/service/ShareholderService.java
@@ -5,10 +5,11 @@ import repository.ShareholderRepository;
 
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ShareholderService {
     private final ShareholderRepository shareholderRepository;
-    private final Scanner scanner=new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public ShareholderService(ShareholderRepository shareholderRepository) {
         this.shareholderRepository = shareholderRepository;
@@ -22,20 +23,29 @@ public class ShareholderService {
             System.out.println("--Error404--");
 
     }
-    public void update(int idShareholder) throws SQLException{
+
+    public void update(int idShareholder) throws SQLException {
         System.out.println("Enter new name :");
-        String name= scanner.next();
+        String name = scanner.next();
         System.out.println("Enter new phoneNumber :");
-        String phoneNumber=scanner.next();
+        String phoneNumber = scanner.next();
         System.out.println("Enter new nationalCode : ");
-        String nationalCode =scanner.next();
-        int result =shareholderRepository.updateInfoShareholder(name,phoneNumber,nationalCode,idShareholder);
-        if(result!= 0)
+        String nationalCode = scanner.next();
+        int result = shareholderRepository.updateInfoShareholder(name, phoneNumber, nationalCode, idShareholder);
+        if (result != 0)
             System.out.println("successfully edited to database");
         else {
             System.out.println("Eror404");
         }
+    }
 
+    public void delete(int idShareholder) throws SQLException {
+        int result = shareholderRepository.delete(idShareholder);
+        if (result != 0) {
+            System.out.println("successfully deleted from database");
+        } else {
+            System.out.println("---Eror404--");
+        }
 
     }
 }
