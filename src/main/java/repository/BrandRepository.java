d187d724156395c95160f3408c4caaf3144eedab
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-20 17:06:59 +0330

    Write ShareholderBrand method

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -13,32 +13,43 @@ public class BrandRepository {
     public BrandRepository(Connection connection) {
         this.connection = connection;
     }
+
     public int save(Brand brand) throws SQLException {
-        String sql="INSERT INTO brand(name, website, description) VALUES (?,?,?)";
-        PreparedStatement preparedStatement= connection.prepareStatement(sql);
-        preparedStatement.setString(1,"name");
-        preparedStatement.setString(2,"website");
-        preparedStatement.setString(3,"description");
-        int result =preparedStatement.executeUpdate();
+        String sql = "INSERT INTO brand(name, website, description) VALUES (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, "name");
+        preparedStatement.setString(2, "website");
+        preparedStatement.setString(3, "description");
+        int result = preparedStatement.executeUpdate();
         return result;
     }
 
-    public int update(String name,String website,String description,int idBrand) throws SQLException {
-        String sql="UPDATE brand SET name=?,website=?,description=? WHERE idbrand=?";
-        PreparedStatement preparedStatement=connection.prepareStatement(sql);
-        preparedStatement.setString(1,name);
-        preparedStatement.setString(2,website);
-        preparedStatement.setString(3,description);
-        preparedStatement.setInt(4,idBrand);
-        int result =preparedStatement.executeUpdate();
+    public int update(String name, String website, String description, int idBrand) throws SQLException {
+        String sql = "UPDATE brand SET name=?,website=?,description=? WHERE idbrand=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, website);
+        preparedStatement.setString(3, description);
+        preparedStatement.setInt(4, idBrand);
+        int result = preparedStatement.executeUpdate();
         return result;
     }
-    public int delete(int idBrand)throws SQLException {
+
+    public int delete(int idBrand) throws SQLException {
         String sql = "DELETE FROM brand where idbrand=?";
-        PreparedStatement preparedStatement=connection.prepareStatement(sql);
-        preparedStatement.setInt(1,idBrand);
-        int result =preparedStatement.executeUpdate();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, idBrand);
+        int result = preparedStatement.executeUpdate();
         return result;
 
+    }
+
+    public int shareholderBrand(int idBrand, int idShareholder) throws SQLException {
+        String sql = "INSERT INTO sharehoderbrand(idbarand, idshareholder) VALUES (?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, idBrand);
+        preparedStatement.setInt(2, idShareholder);
+        int result = preparedStatement.executeUpdate();
+        return result;
     }
 }
