de6b2750c7bc97cdfc19a061bc6ce260bbf86008
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-20 13:13:53 +0330

    Write static ApplicationContex

--- src/main/java/utility/ApplicationContex.java
+++ src/main/java/utility/ApplicationContex.java
@@ -1,8 +1,10 @@
 package utility;
 
 import connection.JdbcConnetion;
+import repository.BrandRepository;
 import repository.ShareholderRepository;
 import repository.UserRepository;
+import service.BrandService;
 import service.ShareholderService;
 import service.UserService;
 
@@ -15,6 +17,8 @@ public class ApplicationContex {
     private final static UserService USER_SERVICE;
     private final static ShareholderRepository SHAREHOLDER_REPOSITORY;
     private final static ShareholderService SHAREHOLDER_SERVICE;
+    private final static BrandRepository BRAND_REPOSITORY;
+    private final static BrandService BRAND_SERVICE;
 
     static {
         CONNECTION =JdbcConnetion.getConnection();
@@ -22,12 +26,17 @@ public class ApplicationContex {
         USER_SERVICE =new UserService(USER_REPOSITORY);
         SHAREHOLDER_REPOSITORY=new ShareholderRepository(CONNECTION);
         SHAREHOLDER_SERVICE=new ShareholderService(SHAREHOLDER_REPOSITORY);
+        BRAND_REPOSITORY=new BrandRepository(CONNECTION);
+        BRAND_SERVICE=new BrandService(BRAND_REPOSITORY);
 
     }
     public static UserService getUserService(){
         return USER_SERVICE;
     }
     public static ShareholderService getShareholderService(){return SHAREHOLDER_SERVICE;}
+    public static BrandService getBrandService(){
+        return BRAND_SERVICE;
+    }
 
 
 
