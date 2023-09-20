465d3626123d48576e546d7a7d238cd7134b6a94
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-20 18:49:40 +0330

    create main class and create object from menu class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import menu.Menu;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Menu menu = new Menu();
+        menu.firstMenu();
+    }
+}
