610c737e88739ab4249dd2db847cffd6b384dfa7
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-20 14:11:49 +0330

    create CategoryRepository and write save,update,delete methods

---
+++ src/main/java/repository/CategoryRepository.java
@@ -0,0 +1,41 @@
+package repository;
+
+import models.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CategoryRepository {
+
+    private final Connection connection;
+
+    public CategoryRepository(Connection connection) {
+        this.connection = connection;
+    }
+    public int save(Category category) throws SQLException {
+        String sql="INSERT INTO category(name, description) VALUES (?,?)";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1,"name");
+        preparedStatement.setString(2,"description");
+        int result =preparedStatement.executeUpdate();
+        return result;
+    }
+    public int update(String name,String description,int idCategory) throws SQLException {
+        String sql="UPDATE category SET name=?,description=? WHERE idcategory=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,description);
+        preparedStatement.setInt(3,idCategory);
+        int result =preparedStatement.executeUpdate();
+        return result;
+    }
+    public int delete(int idCategory)throws SQLException {
+        String sql = "DELETE FROM brand where idbrand=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setInt(1,idCategory);
+        int result =preparedStatement.executeUpdate();
+        return result;
+
+    }
+}
